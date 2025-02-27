b9575bfcdf27a861159925e1ae02c63225f79d02
hdhillon23 <harvird106@deltalearns.ca>
2025-02-27 21:04:57 +0000

    Added 1 more line of pseudocode and a loop that prints a message for each guest invited.

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ guest = []
 # Ask the user how many people they would like to add to the list
 num_people = int(input(f"{user_name} how many people would you like to invite (number only)? "))
 
-# loop that runs until the num_people that you invited in the list are over
+# Loop that runs until the num_people that you invited in the list are over
 for num in range(num_people):
 
     # Adds the guests name in the guest list
     guest_name = input("Whats the name of the guest you want to invite? ")
     guest.append(guest_name)
     print("Name added!")
+
+# Loop that prints a message for each guest
+for num in range(num_people):
+    print(f"Here is an invitation message from {user_name} for {guest_name}: ")
+    print(f"Welcome {guest_name} you have been invited to {user_name}'s dinner party")
     
