7540cf4656c3b80429b00b8d1123d427ef581330
hdhillon23 <harvird106@deltalearns.ca>
2025-02-27 20:57:38 +0000

    Fixed 1 line and added 2 more lines of pseudocode and made a loop that keeps on running until the number of people in the list you invited are over and you can add guests names in the list.

--- main.py
+++ main.py
@@ -4,9 +4,17 @@
 user_name = input("What is you name? ").title().strip()
 print(f"Welcome {user_name}, this is a dinner inviting list program!")
 
-# Make an empty list
+# Create an empty guest list
 guest = []
 
 # Ask the user how many people they would like to add to the list
 num_people = int(input(f"{user_name} how many people would you like to invite (number only)? "))
 
+# loop that runs until the num_people that you invited in the list are over
+for num in range(num_people):
+
+    # Adds the guests name in the guest list
+    guest_name = input("Whats the name of the guest you want to invite? ")
+    guest.append(guest_name)
+    print("Name added!")
+    
