112c4da6f50ad789597ca7017f8250a17808eabe
hdhillon23 <harvird106@deltalearns.ca>
2025-02-27 21:30:30 +0000

    working on if the user wants to reove a guest from the list.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Function that greets the user and ask for their name
 user_name = input("What is you name? ").title().strip()
 print(f"Welcome {user_name}, this is a dinner inviting list program!")
+print("Lets start this Taco Party!!!")
 
 # Create an empty guest list
 guest = []
@@ -18,8 +19,20 @@ for num in range(num_people):
     guest.append(guest_name)
     print("Name added!")
 
-# Loop that prints a message for each guest
+# Loop that prints a message for each guest invited
 for num in range(num_people):
     print(f"Here is an invitation message from {user_name} for {guest_name}: ")
     print(f"Welcome {guest_name} you have been invited to {user_name}'s dinner party")
-    
+
+# Remove guest from the list
+while True:
+    boot = input("Would you like to remove any guest from the list? [y] or [n]")
+    if boot == 'y':
+        eliminate = input("Please enter the name of the person you want to kick out of the Taco Party: ")
+        if eliminate in guest:
+            guest.remove(eliminate)
+            print(f"{eliminate} has been removed from the list for the Taco Party!")
+            print(guest)
+        else:
+            print("This given guest is already not in the list")
+    elif boot == 'n':
